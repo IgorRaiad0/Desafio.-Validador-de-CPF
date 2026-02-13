7005de35953710091727b443a9217c753c4ff4df
IgorRaiad0 <igorsonic56@gmail.com>
2026-02-13 00:40:12 -0300

    Subindo desafio

---
+++ cpf.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+void main(){
+    int cpf[11];
+    int d1, d2;
+    int soma = 0;
+    int i;
+    for(i=0;i<11;i++){
+        scanf("%d", &cpf[i]);
+    }
+    int a = cpf[9];
+    int b = cpf[10];
+    //CÁLCULO DO DÍGITO 1
+    int s = 2;
+    for(i=8;i>=0;i--){
+        soma += (cpf[i] * s);
+        s++;
+    }
+    int resto = soma % 11;
+    if(resto < 2){
+        d1 = 0;
+    }else{
+        d1 = 11 - resto;
+    }
+    cpf[9] = d1;
+
+    //CÁLCULO DO DÍGITO 2
+    soma = 0;
+    s = 2;
+    for(i=9;i>=0;i--){
+        soma += (cpf[i] * s);
+        s++;
+    }
+    resto = soma % 11;
+    if(resto < 2){
+        d2 = 0;
+    }else{
+        d2 = 11 - resto;
+    }
+
+    if(d1 == a && d2 == b){
+        printf("CPF Valido!");
+    }else{
+        printf("CPF Invalido!");
+    }
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
